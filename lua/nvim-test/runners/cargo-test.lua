d38b44d2918ab69f53fda885c8acc0549809751e
Grégory Bataille <gregory.bataille@gmail.com>
2022-12-03 18:34:36 +0100

    handle rust #[test]

--- lua/nvim-test/runners/cargo-test.lua
+++ lua/nvim-test/runners/cargo-test.lua
@@ -1,23 +1,23 @@
--- TODO
---
+local ts = vim.treesitter
+local ts_parsers = require("nvim-treesitter.parsers")
+local ts_utils = require "nvim-treesitter.ts_utils"
+
 local Runner = require "nvim-test.runner"
 
 local cargotest = Runner:init({ command = "cargo", args = { "test" }, package = false }, {
   rust = [[
-      (
-        (
-          mod_item name: (identifier) @test-name
-          (#match? @test-name "[Tt]est")
-        )
-      @scope-root)
+  (
+    mod_item name: (identifier) @mod-name
+    (#match? @mod-name "[Tt]est")
+  )
 
-      (
-        (
-          function_item name: (identifier) @test-name
-          (#match? @test-name "[Tt]est")
-        )
-      @scope-root)
-    ]],
+  (
+    (
+      (attribute_item (attribute (identifier) @attr-name))
+      (function_item name: (identifier) @test-name) @fun
+    ) @scope-root
+  )
+  ]]
 })
 
 function cargotest:build_args(args, filename, opts)
@@ -55,4 +55,46 @@ function cargotest:build_args(args, filename, opts)
   end
 end
 
+function cargotest:find_nearest_test(filetype)
+  local query = ts.get_query(ts_parsers.ft_to_lang(filetype), "nvim-test")
+
+  local found_func = false
+  local found_test = false
+  local found_mod = false
+  local result = {}
+  local curnode = ts_utils.get_node_at_cursor()
+  if not curnode then
+    return result
+  end
+
+  local curLine, _, _, _ = curnode:range()
+
+  while curnode do
+    for capture_id, capture_node, _ in query:iter_captures(curnode, 0) do
+      local start_node, _, end_node, _ = capture_node:range()
+      if query.captures[capture_id] == "fun" and start_node <= curLine and
+          end_node >= curLine then
+        found_func = true
+      end
+
+      if query.captures[capture_id] == "test-name" and found_func and not found_test then
+        local name = self:parse_testname(ts.query.get_node_text(capture_node, 0))
+        table.insert(result, name)
+        found_test = true
+      end
+
+      if query.captures[capture_id] == "mod-name" and not found_mod then
+        local name = self:parse_testname(ts.query.get_node_text(capture_node, 0))
+        table.insert(result, 1, name)
+        found_mod = true
+      end
+    end
+
+    curnode = curnode:parent()
+  end
+
+  return result
+end
+
 return cargotest
+
